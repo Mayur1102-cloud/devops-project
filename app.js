c96292635edf38051de2c39072c02f3bd6306a2d
mayurkadam1102 <mmkadam1999@gmail.com>
2026-03-23 15:22:10 +0530

    fixed binding issue

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello DevOps from AWS 🚀');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });

81c052b19ce9987ca818adbf495f97f02c596fe8
mayurkadam1102 <mmkadam1999@gmail.com>
2026-03-23 15:51:29 +0530

    few lines updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Mayur and akash from ESS 🚀');
+  res.send('Hello Mayur and akash from ESS thanks for watching us 🚀');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
